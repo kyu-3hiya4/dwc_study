bda318da36efc7a50154d831c768ccf0ddfabe0a
kyu-3hiya4 <177545609+kyu-3hiya4@users.noreply.github.com>
2025-03-31 12:01:49 +0000

    17日目

--- java/practice-variable/practice-variable/sample.js
+++ java/practice-variable/practice-variable/sample.js
@@ -1,4 +1,9 @@
-var nickname = "taro"
-console.log(nickname)
-var nickname = "ichiro"
-console.log(nickname)
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
